ab598069578b00197d9f9a799a19878cf01eb592
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2019-09-08 11:23:35 +0900

    Rewrite example of foldl

---
+++ examples/strict-gotchas/stackoverflow.hs
@@ -0,0 +1,15 @@
+-- The result of this example doesn't change whether with or without Strict.
+{-
+  stack exec ghc -- -O0 -rtsopts -with-rtsopts=-K1k          .\folding.hs
+  stack exec ghc -- -O0 -rtsopts -with-rtsopts=-K1k -XStrict .\folding.hs
+-}
+
+import Control.Exception
+
+main :: IO ()
+main = do
+  let size = 50000
+  evaluate $ foldr (\x z -> x : z) [] [1 .. size]
+  putStrLn "DONE: foldr"
+  evaluate $ foldl (\z x -> x : z) [] [1 .. size]
+  putStrLn "DONE: foldl"
