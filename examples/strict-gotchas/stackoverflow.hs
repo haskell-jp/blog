3dfa7173580cc6be8114eb56b4d6c05dff51dbc9
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2019-09-10 22:16:19 +0900

    Complete example of stackoverflow.hs

--- examples/strict-gotchas/stackoverflow.hs
+++ examples/strict-gotchas/stackoverflow.hs
@@ -1,15 +1,23 @@
--- The result of this example doesn't change whether with or without Strict.
 {-
   stack exec ghc -- -O0 -rtsopts -with-rtsopts=-K1k          ./stackoverflow.hs
   stack exec ghc -- -O0 -rtsopts -with-rtsopts=-K1k -XStrict ./stackoverflow.hs
+  stack exec ghc -- -O1 -rtsopts -with-rtsopts=-K1k          ./stackoverflow.hs
+  stack exec ghc -- -O1 -rtsopts -with-rtsopts=-K1k -XStrict ./stackoverflow.hs
 -}
 
 import Control.Exception
+import Data.List
 
 main :: IO ()
 main = do
-  let size = 50000
-  evaluate $ foldr (\x z -> x : z) [] [1 .. size]
-  putStrLn "DONE: foldr"
+  let size = 5000
+  putStrLn "BEGIN"
+
+  evaluate $ foldr (:) [] [1 .. size]
+  putStrLn "DONE: foldr 1"
+
   evaluate $ foldl (\z x -> x : z) [] [1 .. size]
   putStrLn "DONE: foldl"
+
+  evaluate $ foldr (\x z -> x : z) [] [1 .. size]
+  putStrLn "DONE: foldr 2"
